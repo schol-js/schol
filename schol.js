082236adbc431d6704a3821c41e2162f0e87fd14
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2018-10-03 07:05:42 -0600

    Improve

--- schol.js
+++ schol.js
@@ -6,15 +6,54 @@
 
 var schol = require('commander');
 
-const package = require('./package.json');
+const pkg = require('./package.json');
 
-schol.version(package.version);
-schol.command('init', 'Initializes a new schol project in the current directory.').action(function (env, options) {
+schol.version(pkg.version);
 
-});
-schol.command('render', 'Generates a distributable and/or publishable version of your project in the docs/ folder.');
-schol.command('edit', 'Opens your project in your web browser and watches for changes to the project. Automatically rebuilds the project and refreshes the browser when changes are detected.');
-schol.command('publish', 'Publishes the most recently rendered version of the project to GitHub Pages.');
+schol
+  .command('init')
+  .description('Initializes a new schol project in the current directory.')
+	.action(function (env, options) {
+		var yeoman = require('yeoman-environment');
+		var yoEnv = yeoman.createEnv();
+		yoEnv.register(require.resolve('generator-schol'), 'schol');
+		yoEnv.run('schol');
+	});
+
+schol
+  .command('edit')
+  .description('Opens your project in your web browser and watches for changes to the project. Automatically rebuilds the project and refreshes the browser when changes are detected.')
+  .action(function (env, options) {
+  	const gulp = require('gulp');
+  	const frontMatter = require('gulp-front-matter');
+  	gulp.src('src/**/*.md')
+  	  .pipe(frontMatter())
+  	  .
+  	const md = require('markdown-it');
+
+  });
+
+schol
+  .command('render')
+  .description('Generates a distributable and/or publishable version of your project in the docs/ folder.')
+  .action(function (env, options) {
+  	// Get all our source files
+  	// Handle documents
+  	// ? Download and install any necessary templates ?
+  	// Parse citations
+  	// Output any other files directly
+  	var md = require('markdown-it')();
+		var result = md.render('# markdown-it rulezz!');
+  });
+;
+
+schol
+  .command('publish')
+  .description('Publishes the most recently rendered version of the project to GitHub Pages.')
+  .action(function (env, options) {
+		console.log('test')
+	});
+
 schol.parse(process.argv);
 
 module.exports = schol;
